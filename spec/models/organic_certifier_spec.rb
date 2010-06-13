86f737def215b8455eb16b2fadc21c90b87c8ee4
Al Zimmerman <auraelius@gmail.com>
2010-06-13 08:15:37 -0700

    added organic_certifier and washing_station validations

--- spec/models/organic_certifier_spec.rb
+++ spec/models/organic_certifier_spec.rb
@@ -10,17 +10,45 @@
 #  updated_at :datetime
 #
 
+=begin
+  TODO check to make sure code validations work
+=end
+
 require 'spec_helper'
 
 describe OrganicCertifier do
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
-      :code => "value for code"
+      :code => "XXX"
     }
   end
 
   it "should create a new instance given valid attributes" do
     OrganicCertifier.create!(@valid_attributes)
+  end
+  
+  it "should require a name" do
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:name => ""))
+    no_name_organic_certifier.should_not be_valid
+  end
+
+  it "should require a code" do
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:code => ""))
+    no_name_organic_certifier.should_not be_valid
+  end
+  
+  it "should require a three character code" do
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:code => "X"))
+    no_name_organic_certifier.should_not be_valid
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:code => "XX"))
+    no_name_organic_certifier.should_not be_valid
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:code => "XXXX"))
+    no_name_organic_certifier.should_not be_valid
+  end
+
+  it "should require an uppercase code" do
+    no_name_organic_certifier = OrganicCertifier.new(@valid_attributes.merge(:code => "xxx"))
+    no_name_organic_certifier.should_not be_valid
   end
 end
--- spec/models/washing_station_spec.rb
+++ spec/models/washing_station_spec.rb
@@ -10,17 +10,49 @@
 #  updated_at :datetime
 #
 
+=begin
+  TODO check to make sure code validations work
+=end
+
 require 'spec_helper'
 
 describe WashingStation do
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
-      :code => "value for code"
+      :code => "XXX"
     }
   end
 
   it "should create a new instance given valid attributes" do
     WashingStation.create!(@valid_attributes)
   end
+  
+  it "should require a name" do
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:name => ""))
+    no_name_washing_station.should_not be_valid
+  end
+
+  it "should require a code" do
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:code => ""))
+    no_name_washing_station.should_not be_valid
+  end
+  
+  it "should require a three character code" do
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:code => "X"))
+    no_name_washing_station.should_not be_valid
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:code => "XX"))
+    no_name_washing_station.should_not be_valid
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:code => "XXXX"))
+    no_name_washing_station.should_not be_valid
+    # TODO is it safe to assume other string lengths fail?
+  end
+
+  it "should require an uppercase code" do
+    no_name_washing_station = WashingStation.new(@valid_attributes.merge(:code => "xxx"))
+    no_name_washing_station.should_not be_valid
+  end  
+  
+  
+  
 end
